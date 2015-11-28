c7c5d8fb17f1e7ea0340c94939883328f917efc1
Julien Laloi <julien.laloi@gmail.com>
2015-11-28 20:00:31 +0100

    Roles

--- src/main/java/fr/julien/springbootldap/LdapSecurityConfiguration.java
+++ src/main/java/fr/julien/springbootldap/LdapSecurityConfiguration.java
@@ -1,29 +1,32 @@
 package fr.julien.springbootldap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by julien on 28/11/15.
  */
 @Configuration
-@EnableWebSecurity
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class LdapSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.ldapAuthentication().contextSource(contextSource()).userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").userSearchBase("ou=people");
+        auth.ldapAuthentication().contextSource(contextSource()).userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").groupSearchFilter("member={0}").userSearchBase("ou=people");
     }
 
     @Bean
-    @ConfigurationProperties(prefix="ldap.contextSource")
+    @ConfigurationProperties(prefix = "ldap.contextSource")
     public LdapContextSource contextSource() {
         return new LdapContextSource();
     }
--- src/main/java/fr/julien/springbootldap/RestServices.java
+++ src/main/java/fr/julien/springbootldap/RestServices.java
@@ -1,9 +1,14 @@
 package fr.julien.springbootldap;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.Collection;
 
 /**
  * Created by julien on 28/11/15.
@@ -12,13 +17,26 @@ import java.security.Principal;
 public class RestServices {
 
     @RequestMapping("/")
-    public String index() {
-        return "Hello!";
+    public String index(Principal user) {
+        return "Welcome " + user.getName() + "!";
     }
 
     @RequestMapping("/logged")
     public Principal user(Principal user) {
         return user;
+    }
+
+    @RequestMapping("/roles")
+    public Collection<? extends GrantedAuthority> roles(Principal principal) {
+        UserDetails currentUser = (UserDetails) ((Authentication) principal).getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = currentUser.getAuthorities();
+        return authorities;
+    }
+
+    @Secured("ROLE_DEVELOPERS")
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello is granted! ;)";
     }
 
 }
