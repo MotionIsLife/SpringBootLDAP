d59a25370b59af0a4f3ea3486cdf0e01705375b6
Julien Laloi <julien.laloi@gmail.com>
2015-11-29 10:23:42 +0100

    @ConfigurationProperties for ldap configuration

--- src/main/java/fr/julien/springbootldap/LdapSecurityConfiguration.java
+++ src/main/java/fr/julien/springbootldap/LdapSecurityConfiguration.java
@@ -21,8 +21,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class LdapSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    private ContextConfiguration contextConfiguration;
+
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.ldapAuthentication().contextSource(contextSource()).userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").groupSearchFilter("member={0}").userSearchBase("ou=people");
+        auth.ldapAuthentication().contextSource(contextSource())
+                .userDnPatterns(contextConfiguration.getUserDnPatterns())
+                .groupSearchBase(contextConfiguration.getGroupSearchBase())
+                .groupSearchFilter(contextConfiguration.getGroupSearchFilter())
+                .userSearchBase(contextConfiguration.getUserSearchBase());
     }
 
     @Bean
@@ -33,11 +40,9 @@ public class LdapSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/css/**").permitAll()
-                .anyRequest().fullyAuthenticated()
-                .and()
-                .formLogin();
+        http.authorizeRequests()
+            .anyRequest().fullyAuthenticated()
+            .and()
+            .formLogin();
     }
 }
